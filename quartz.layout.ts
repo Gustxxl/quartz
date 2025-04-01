fd86f9da59aad09c24b1dd0285e631b558fed528
Vladislav Balabaev <givemeagust@gmail.com>
2025-04-01 16:41:21 +0300

    Quartz sync: Apr 1, 2025, 4:41 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/Gustxxl",
-      "Telegram": "t.me/Gustxxl",
+      "Telegram": "https://t.me/Gustxxl",
     },
   }),
 }
